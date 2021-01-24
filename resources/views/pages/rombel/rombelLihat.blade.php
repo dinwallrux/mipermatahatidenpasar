586aef2eba05cb18230dd866198d185e8f65d5ec
Salahuddin <sholahudinbest@gmail.com>
2021-01-25 00:31:04 +0800

    Add tombol kembali di rombel
    Add tombol kembali di rombel

--- resources/views/pages/rombel/rombelLihat.blade.php
+++ resources/views/pages/rombel/rombelLihat.blade.php
@@ -8,6 +8,10 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Data Siswa {{$data->nama_rombel}}</h6>
+                <a href="{{route('rombel')}}" class="back text-danger" style="text-decoration: none;">
+                    <i class="far fa-arrow-alt-circle-left"></i>
+                    <span>Kembali</span>
+                </a>
             </div>
             <div class="card-body">
                 
