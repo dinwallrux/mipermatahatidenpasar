a574f7a599ad40fcfef54638705b6c91244cc6c9
Salahuddin <sholahudinbest@gmail.com>
2021-02-07 20:01:59 +0800

    Change image login
    Change image login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block login-image" style="background-image: url({{asset('main/img/login-bg.jpg')}})"></div>
+                    <div class="col-lg-6 d-none d-lg-block login-image" style="background-image: url({{asset('main/img/logo.png')}})"></div>
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
