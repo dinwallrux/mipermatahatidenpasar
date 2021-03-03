5b3970e6e295f2e19369f9c7cfa865dcff2b41ee
Salahuddin <sholahudinbest@gmail.com>
2021-03-04 00:12:37 +0800

    - Ubah size login - Hapus register link in login page
    - Ubah size login
    - Hapus register link in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,17 @@
 @section('contentAuth')
 <div class="row justify-content-center" id="login">
 
-    <div class="col-xl-10 col-lg-12 col-md-9">
+    <div class="col-xl-6 col-lg-12 col-md-9">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block login-image" style="background-image: url({{asset('main/img/logo.png')}})"></div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
+                    <div class="col-lg-12">
+                        <div class="pt-4 pr-5 pl-5 pb-5">
+                            <div class="text-center mb-4">
+                                <img src="{{asset('main/img/logo.png')}}" alt="" width="180">
+                            </div>
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                             </div>
@@ -42,10 +44,6 @@
                                     Login
                                 </button>
                             </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="{{ route('register') }}">Buat Akun</a>
-                            </div>
                         </div>
                     </div>
                 </div>
